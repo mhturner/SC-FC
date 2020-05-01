a7b563b1d65c6e18294de01892cc8e66a3ccd29c
Max <maxwellholteturner@gmail.com>
2020-05-01 13:08:59 -0700

    JOb script

---
+++ compute_connectivity_matrix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=comp_conn_mat
+#SBATCH --partition=trc
+#SBATCH --time=4:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output=/home/users/mhturner/SC-FC/job_outputs/%x.%j.out
+#SBATCH --open-mode=append
+
+ml python/3.6.1
+
+python3 /home/users/mhturner/SC-FC/compute_connectivity_matrix.py
