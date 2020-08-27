e8149d4660d5f483b70a68e9fe40f50a33a328a1
Max <maxwellholteturner@gmail.com>
2020-08-27 11:58:46 -0700

    pkg reorg, remove old shit

--- scratch.py
+++ scratch.py
@@ -1,27 +1,29 @@
-import svgutils.transform as sg
+import matplotlib.pyplot as plt
+from neuprint import Client
+import numpy as np
 import os
+from scipy.stats import zscore
+import pandas as pd
+import seaborn as sns
 
+from scfc import bridge, anatomical_connectivity, functional_connectivity, plotting
+import matplotlib
+from matplotlib import rcParams
+rcParams.update({'font.size': 12})
+rcParams.update({'figure.autolayout': True})
 
-data_dir = '/home/mhturner/Dropbox/ClandininLab/Analysis/SC-FC'
+data_dir = '/home/mhturner/Dropbox/ClandininLab/Analysis/SC-FC/data'
+analysis_dir = '/home/mhturner/Dropbox/ClandininLab/Analysis/SC-FC'
 
+# start client
+neuprint_client = Client('neuprint.janelia.org', dataset='hemibrain:v1.1', token=bridge.getNeuprintToken())
 
-fig = sg.SVGFigure("8.5in", "11in")
+# Get FunctionalConnectivity object
+FC = functional_connectivity.FunctionalConnectivity(data_dir=data_dir, fs=1.2, cutoff=0.01, mapping=bridge.getRoiMapping())
 
+# Get AnatomicalConnectivity object
+AC = anatomical_connectivity.AnatomicalConnectivity(data_dir=data_dir, neuprint_client=neuprint_client, mapping=bridge.getRoiMapping())
 
-fig1 = sg.fromfile(os.path.join(data_dir, 'figpanels', 'Fig8.svg'))
-fig2 = sg.fromfile(os.path.join(data_dir, 'figpanels', 'Fig12.svg'))
+plot_colors = plt.get_cmap('tab10')(np.arange(8)/8)
 
-
-fig1.getroot().moveto(0, 0, scale=1)
-fig2.getroot().moveto(305, 0, scale=1)
-
-# add text labels
-txt1 = sg.TextElement(25,20, "A", size=12, weight="bold")
-txt2 = sg.TextElement(305,20, "B", size=12, weight="bold")
-
-# append plots and labels to figure
-fig.append([fig1.getroot(), fig2.getroot()])
-fig.append([txt1, txt2])
-
-# save generated SVG files
-fig.save(os.path.join(data_dir, 'svg_figs', 'Fig3.svg'))
+# %%
