5ce34e73e4b35032ce25f0854f0d9a8126d59036
Max <maxwellholteturner@gmail.com>
2020-10-15 11:03:15 -0700

    F1 change QQ plots to separate panels rather than insets

--- figures/figure_1.py
+++ figures/figure_1.py
@@ -154,41 +154,37 @@ for iter in range(100):
     theory_distr.append(norm_model.rvs(size=len(data)))
 theory_distr = np.vstack(theory_distr)
 
-fig1_1, ax = plt.subplots(1, 1, figsize=(3.5, 2.25))
+fig1_1, ax = plt.subplots(1, 2, figsize=(5.0, 2.25))
 val, bin = np.histogram(data, 20, density=True)
 bin_ctrs = bin[:-1]
 xx = np.linspace(-3.5, 3.5)
-ax.plot(10**xx, norm_model.pdf(xx), linewidth=2, color='k', linestyle='--')
-ax.plot(10**bin_ctrs, val, linewidth=3)
-ax.set_xscale('log')
-ax.set_xlabel('Cells (z-score)')
-ax.set_ylabel('Prob.')
-ax.set_xticks([1e-2, 1, 1e2])
+ax[0].plot(10**xx, norm_model.pdf(xx), linewidth=2, color='k', linestyle='--')
+ax[0].plot(10**bin_ctrs, val, linewidth=3)
+ax[0].set_xscale('log')
+ax[0].set_xlabel('Cells (z-score)')
+ax[0].set_ylabel('Prob.')
+ax[0].set_xticks([1e-2, 1, 1e2])
 
 
 # Q-Q plot of log-transformed data vs. fit normal distribution
-fig1_2, ax = plt.subplots(1, 1, figsize=(1.2, 1.2))
-ax.plot([10**-4, 10**4], [10**-4, 10**4], 'k-')
+ax[1].plot([10**-4, 10**4], [10**-4, 10**4], 'k-')
 quants = np.linspace(0, 1, 20)
 for q in quants:
     th_pts = np.quantile(theory_distr, q, axis=1)  # quantile value for each iteration
-    ax.plot([10**np.quantile(data, q), 10**np.quantile(data, q)], [10**(np.mean(th_pts) - 2*np.std(th_pts)), 10**(np.mean(th_pts) + 2*np.std(th_pts))], color=plot_colors[0], alpha=1)
-    ax.plot(10**np.quantile(data, q), 10**np.mean(th_pts), marker='o', color=plot_colors[0], alpha=1)
-# ax.set_xlabel('Cells', fontsize=8)
-# ax.set_ylabel('Lognorm.', fontsize=8)
-ax.set_xscale('log')
-ax.set_yscale('log')
+    ax[1].plot([10**np.quantile(data, q), 10**np.quantile(data, q)], [10**(np.mean(th_pts) - 2*np.std(th_pts)), 10**(np.mean(th_pts) + 2*np.std(th_pts))], color=plot_colors[0], alpha=1)
+    ax[1].plot(10**np.quantile(data, q), 10**np.mean(th_pts), marker='o', color=plot_colors[0], alpha=1)
+ax[1].set_xlabel('Q. Measured')
+ax[1].set_ylabel('Q. Lognorm.')
+ax[1].set_xscale('log')
+ax[1].set_yscale('log')
 
-ax.set_xticks([1e-2, 1e2])
-ax.set_yticks([1e-2, 1e2])
-ax.set_xticklabels(['-2$\sigma$', '+2$\sigma$'])
-ax.set_yticklabels(['-2$\sigma$', '+2$\sigma$'])
-ax.axhline(y=1, color='k', zorder=0, alpha=0.5)
-ax.axvline(x=1, color='k', zorder=0, alpha=0.5)
-for tick in ax.xaxis.get_major_ticks():
-    tick.label.set_fontsize(8)
-for tick in ax.yaxis.get_major_ticks():
-    tick.label.set_fontsize(8)
+ax[1].set_xticks([1e-2, 1e2])
+ax[1].set_yticks([1e-2, 1e2])
+ax[1].set_xticklabels(['-2$\sigma$', '+2$\sigma$'])
+ax[1].set_yticklabels(['-2$\sigma$', '+2$\sigma$'])
+ax[1].axhline(y=1, color='k', zorder=0, alpha=0.5)
+ax[1].axvline(x=1, color='k', zorder=0, alpha=0.5)
+
 
 # # # # TBARS # # # # # # # # # # # # # # # #:
 p_vals = []
@@ -206,48 +202,42 @@ for iter in range(100):
     theory_distr.append(norm_model.rvs(size=len(data)))
 theory_distr = np.vstack(theory_distr)
 
-fig1_3, ax = plt.subplots(1, 1, figsize=(3.5, 2.25))
+fig1_2, ax = plt.subplots(1, 2, figsize=(5.0, 2.25))
 val, bin = np.histogram(data, 20, density=True)
 bin_ctrs = bin[:-1]
 xx = np.linspace(-3.5, 3.5)
-ax.plot(10**xx, norm_model.pdf(xx), linewidth=2, color='k', linestyle='--')
-ax.plot(10**bin_ctrs, val, linewidth=3)
-ax.set_xscale('log')
-ax.set_xlabel('T-Bars (z-score)')
-ax.set_ylabel('Prob.')
-ax.set_xticks([1e-2, 1, 1e2])
+ax[0].plot(10**xx, norm_model.pdf(xx), linewidth=2, color='k', linestyle='--')
+ax[0].plot(10**bin_ctrs, val, linewidth=3)
+ax[0].set_xscale('log')
+ax[0].set_xlabel('T-Bars (z-score)')
+ax[0].set_ylabel('Prob.')
+ax[0].set_xticks([1e-2, 1, 1e2])
 
 
 # Q-Q plot of log-transformed data vs. fit normal distribution
-fig1_4, ax = plt.subplots(1, 1, figsize=(1.2, 1.2))
-ax.plot([10**-4, 10**4], [10**-4, 10**4], 'k-')
+ax[1].plot([10**-4, 10**4], [10**-4, 10**4], 'k-')
 quants = np.linspace(0, 1, 20)
 for q in quants:
     th_pts = np.quantile(theory_distr, q, axis=1)  # quantile value for each iteration
-    ax.plot([10**np.quantile(data, q), 10**np.quantile(data, q)], [10**(np.mean(th_pts) - 2*np.std(th_pts)), 10**(np.mean(th_pts) + 2*np.std(th_pts))], color=plot_colors[0], alpha=1)
-    ax.plot(10**np.quantile(data, q), 10**np.mean(th_pts), marker='o', color=plot_colors[0], alpha=1)
-# ax.set_xlabel('Cells', fontsize=8)
-# ax.set_ylabel('Lognorm.', fontsize=8)
-ax.set_xscale('log')
-ax.set_yscale('log')
+    ax[1].plot([10**np.quantile(data, q), 10**np.quantile(data, q)], [10**(np.mean(th_pts) - 2*np.std(th_pts)), 10**(np.mean(th_pts) + 2*np.std(th_pts))], color=plot_colors[0], alpha=1)
+    ax[1].plot(10**np.quantile(data, q), 10**np.mean(th_pts), marker='o', color=plot_colors[0], alpha=1)
+ax[1].set_xlabel('Q. Measured')
+ax[1].set_ylabel('Q. Lognorm.')
+ax[1].set_xscale('log')
+ax[1].set_yscale('log')
 
 
-ax.set_xticks([1e-2, 1e2])
-ax.set_yticks([1e-2, 1e2])
-ax.set_xticklabels(['-2$\sigma$', '+2$\sigma$'])
-ax.set_yticklabels(['-2$\sigma$', '+2$\sigma$'])
-ax.axhline(y=1, color='k', zorder=0, alpha=0.5)
-ax.axvline(x=1, color='k', zorder=0, alpha=0.5)
-for tick in ax.xaxis.get_major_ticks():
-    tick.label.set_fontsize(8)
-for tick in ax.yaxis.get_major_ticks():
-    tick.label.set_fontsize(8)
+ax[1].set_xticks([1e-2, 1e2])
+ax[1].set_yticks([1e-2, 1e2])
+ax[1].set_xticklabels(['-2$\sigma$', '+2$\sigma$'])
+ax[1].set_yticklabels(['-2$\sigma$', '+2$\sigma$'])
+ax[1].axhline(y=1, color='k', zorder=0, alpha=0.5)
+ax[1].axvline(x=1, color='k', zorder=0, alpha=0.5)
+
 
 
 fig1_1.savefig(os.path.join(analysis_dir, 'figpanels', 'fig1_1.svg'), format='svg', transparent=True, dpi=save_dpi)
 fig1_2.savefig(os.path.join(analysis_dir, 'figpanels', 'fig1_2.svg'), format='svg', transparent=True, dpi=save_dpi)
-fig1_3.savefig(os.path.join(analysis_dir, 'figpanels', 'fig1_3.svg'), format='svg', transparent=True, dpi=save_dpi)
-fig1_4.savefig(os.path.join(analysis_dir, 'figpanels', 'fig1_4.svg'), format='svg', transparent=True, dpi=save_dpi)
 
 # %%
 import pandas as pd
