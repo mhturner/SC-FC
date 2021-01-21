219a179d1892856ccb4a6eb32ed0f143813b142f
Max <maxwellholteturner@gmail.com>
2021-01-20 18:19:44 -0800

    supervoxel analysis update

--- supervoxel_conn.py
+++ supervoxel_conn.py
@@ -102,20 +102,13 @@ for c_ind in range(FC.cmats.shape[2]):
 print('r = {:.2f} +/- {:.2f}'.format(np.mean(r_vals), np.std(r_vals)))
 
 # %% Load branson_cellcount_matrix and branson_synmask from R / natverse script
-
 count_matrix_jfrc2 = pd.read_csv(os.path.join(data_dir, 'JFRC2_branson_cellcount_matrix.csv'), header=0).to_numpy()[:, 1:]
 count_matrix_jfrc2 = pd.DataFrame(data=count_matrix_jfrc2, index=np.arange(1, 1000), columns=np.arange(1, 1000))
 
-sns.heatmap(count_matrix_jfrc2)
-
-# %%
 count_matrix_jrc2018 = pd.read_csv(os.path.join(data_dir, 'JRC2018F_branson_cellcount_matrix.csv'), header=0).to_numpy()[:, 1:]
 count_matrix_jrc2018 = pd.DataFrame(data=count_matrix_jrc2018, index=np.arange(1, 1000), columns=np.arange(1, 1000))
 
-sns.heatmap(count_matrix_jrc2018)
-
-# %%filter and sort count_matrix by include_inds
-
+# filter and sort count_matrix by include_inds
 Connectivity_JFRC2 = pd.DataFrame(data=np.zeros_like(mean_cmat), index=name_list, columns=name_list)
 Connectivity_JRC2018 = pd.DataFrame(data=np.zeros_like(mean_cmat), index=name_list, columns=name_list)
 
@@ -125,12 +118,15 @@ for s_ind, src in enumerate(include_inds):
         Connectivity_JRC2018.iloc[s_ind, t_ind] = count_matrix_jrc2018.loc[src, trg]
 
 # %%
-fh1, ax1 = plt.subplots(1, 2, figsize=(8, 4))
-sns.heatmap(Connectivity_JFRC2, ax=ax1[0])
-ax1[0].set_title('JFRC2 atlas')
+fh0, ax0 = plt.subplots(1, 3, figsize=(18, 6))
+sns.heatmap(CorrelationMatrix, ax=ax0[0], cmap='cividis')
+ax0[0].set_title('Fxnal')
 
-sns.heatmap(Connectivity_JRC2018, ax=ax1[1])
-ax1[1].set_title('JRC2018 atlas')
+sns.heatmap(Connectivity_JFRC2, ax=ax0[1], cmap='cividis')
+ax0[1].set_title('JFRC2 atlas')
+
+sns.heatmap(Connectivity_JRC2018, ax=ax0[2], cmap='cividis')
+ax0[2].set_title('JRC2018 atlas')
 
 # %% load synmask tifs
 branson_jfrc2 = io.imread(os.path.join(data_dir, 'AnatomySubCompartments20150108_ms999centers.tif'))
@@ -141,61 +137,18 @@ synmask_jrc2018 = io.imread(os.path.join(data_dir, 'JRC2018F_branson_synmask.tif
 
 # %%
 
-fh1, ax1 = plt.subplots(3, 3, figsize=(18, 9))
-ax1[0, 0].imshow(branson_brain.sum(axis=2))
+fh1, ax1 = plt.subplots(2, 3, figsize=(18, 6))
+ax1[0, 0].imshow(branson_jfrc2.sum(axis=2))
 ax1[1, 0].imshow(synmask_jfrc2.sum(axis=2))
-ax1[2, 0].imshow(synmask_jrc2018.sum(axis=2))
-
-ax1[0, 1].imshow(branson_brain.sum(axis=1))
+ax1[0, 1].imshow(branson_jfrc2.sum(axis=1))
 ax1[1, 1].imshow(synmask_jfrc2.sum(axis=1))
-ax1[2, 1].imshow(synmask_jrc2018.sum(axis=1))
-
-ax1[0, 2].imshow(branson_brain.sum(axis=0))
+ax1[0, 2].imshow(branson_jfrc2.sum(axis=0))
 ax1[1, 2].imshow(synmask_jfrc2.sum(axis=0))
-ax1[2, 2].imshow(synmask_jrc2018.sum(axis=0))
 
-
-# %% OLD SHIT
-
-
-
-# %% Load body_ids that make connections in the Ito, 36 region atlas data
-body_ids = np.load(os.path.join(data_dir, 'connectome_connectivity', 'body_ids_20210114.npy'), allow_pickle=True)
-
-np.savetxt(os.path.join(data_dir, 'body_ids.csv'), body_ids, delimiter=",")
-
-body_ids = np.random.choice(body_ids, 100, replace=False)
-
-
-# %% Go through body_ids cell by cell, figure out which branson atlas voxel contains each pre + post-synapse
-t0 = time.time()
-
-res = 0.68 # um/voxel of atlas
-# res = 2 # um/voxel of atlas
-
-
-regions = np.unique(branson_brain)[1:] # cut out first region (0), which is empty
-
-count_matrix = pd.DataFrame(data=np.zeros((len(regions), len(regions))), index=regions, columns=regions)
-
-syn_mask = np.zeros_like(branson_brain)
-for body in body_ids:
-    Q = """\
-        MATCH (n:Neuron)-[:Contains]->(:SynapseSet)-[:Contains]->(s:Synapse)
-        WHERE n.status = "Traced"
-        AND n.bodyId = {}
-        RETURN DISTINCT [s.location.x * 8/1000, s.location.y * 8/1000, s.location.z * 8/1000] AS LOC, s.type
-
-        """.format(body)
-    df = fetch_custom(Q, format='pandas', client=neuprint_client)
-    if len(df) > 0:
-        inds = np.round(np.vstack(df['LOC']) / res).astype('int') # convert from microns to atlas voxels. Indices in mask where synapses are located
-        df['region'] = branson_brain[inds[:, 1], inds[:, 0], inds[:, 2]] # region number for each row (synapse) associated with this cell
-
-        target_regions = np.unique(df[df['s.type'] == 'pre'].region)
-        source_regions = np.unique(df[df['s.type'] == 'post'].region)
-
-        count_matrix.loc[source_regions[source_regions>0], target_regions[target_regions>0]] += 1
-        syn_mask[inds[:, 1], inds[:, 0], inds[:, 2]] += 1
-
-print('Done ({:.2f} sec)'.format(time.time() - t0))
+fh2, ax2 = plt.subplots(2, 3, figsize=(18, 6))
+ax2[0, 0].imshow(branson_jrc2018.sum(axis=2))
+ax2[1, 0].imshow(synmask_jrc2018.sum(axis=2))
+ax2[0, 1].imshow(branson_jrc2018.sum(axis=1))
+ax2[1, 1].imshow(synmask_jrc2018.sum(axis=1))
+ax2[0, 2].imshow(branson_jrc2018.sum(axis=0))
+ax2[1, 2].imshow(synmask_jrc2018.sum(axis=0))
