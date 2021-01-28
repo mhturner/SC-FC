8d7844a16fd3c26a55bd31e3d93f6ed8a5ff89c7
Max <maxwellholteturner@gmail.com>
2021-01-28 14:59:19 -0800

    test subsamp

--- compute_subsampled_cmats.py
+++ compute_subsampled_cmats.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 import glob
 import datetime
+import pandas as pd
 
 from scfc import functional_connectivity, bridge
 
@@ -28,8 +29,10 @@ include_inds_ito, name_list_ito = bridge.getItoNames()
 
 # Get full cmat, avg across flies
 t0 = time.time()
-cmats = []
-for brain_fp in brain_filepaths:
+cmats_full = []
+cmats_subsampled = []
+for brain_fp in brain_filepaths[0:2]: # TESTING
+    # for brain_fp in brain_filepaths:
     t0 = time.time()
     suffix = brain_fp.split('func_volreg_')[-1]
     file_id = suffix.replace('.nii.gz', '')
@@ -45,45 +48,22 @@ for brain_fp in brain_filepaths:
         new_roi_mask = mask_brain == r # bool
         roi_mask.append(new_roi_mask)
 
+    # (1) Full region responses:
     # region_responses: n_rois x n_timepoints np array, mean voxel response in each region
     region_responses_full = functional_connectivity.computeRegionResponses(functional_brain, roi_mask)
     region_responses_full = functional_connectivity.filterRegionResponse(region_responses_full, cutoff=cutoff, fs=fs)
-    region_responses_full = functional_connectivity.trimRegionResponse(file_id, region_responses_full)
+    RegionResponses = pd.DataFrame(data=functional_connectivity.trimRegionResponse(file_id, region_responses_full), index=rois)
 
     # Trim down to included ito regions
-    resp_included = region_responses_full.reindex(include_inds_ito).to_numpy()
-
+    resp_included = RegionResponses.reindex(include_inds_ito).to_numpy()
     # compute cmat
     correlation_matrix = np.corrcoef(resp_included)
     # set diag to 0
     np.fill_diagonal(correlation_matrix, 0)
     # fischer z transform (arctanh) and append
-    cmats.append(np.arctanh(correlation_matrix))
+    cmats_full.append(np.arctanh(correlation_matrix))
 
-cmats = np.stack(cmats, axis=2) # population cmats, z transformed
-
-CorrelationMatrix_Full = np.mean(cmats, axis=2) # roi x roi
-print('Finished full region cmat (time = {:.1f} sec)'.format(time.time()-t0))
-
-
-# population cmats for subsampled sizes and iterations
-cmats_pop = []
-for brain_fp in brain_filepaths:
-    suffix = brain_fp.split('func_volreg_')[-1]
-    print('Starting brain {}'.format(suffix))
-    t0 = time.time()
-    atlas_fp = os.path.join(data_dir, 'ito_68_atlas', 'vfb_68_' + suffix)
-
-    mask_brain = np.asarray(np.squeeze(nib.load(atlas_fp).get_fdata()), 'uint16')
-    functional_brain = np.asanyarray(nib.load(brain_fp).dataobj).astype('uint16')
-
-    rois = np.unique(mask_brain) # roi ID numbers
-    roi_mask = []
-    for r_ind, r in enumerate(rois):
-        new_roi_mask = np.zeros_like(mask_brain)
-        new_roi_mask = mask_brain == r # bool
-        roi_mask.append(new_roi_mask)
-
+    # (2) Subsampled regions
     cmats_sizes = []
     for subsampled_size in subsampled_sizes:
         cmats_iter = []
@@ -101,8 +81,12 @@ for brain_fp in brain_filepaths:
             # get region responses and filter, trim
             region_responses_subsampled = functional_connectivity.filterRegionResponse(np.vstack(region_responses_subsampled), cutoff=cutoff, fs=fs)
             region_responses_subsampled = functional_connectivity.trimRegionResponse(file_id, region_responses_subsampled)
+            RegionResponses = pd.DataFrame(data=functional_connectivity.trimRegionResponse(file_id, region_responses_subsampled), index=rois)
 
-            correlation_matrix = np.corrcoef(region_responses_subsampled) # roi x roi
+            # Trim down to included ito regions
+            resp_included = RegionResponses.reindex(include_inds_ito).to_numpy()
+            # compute cmat
+            correlation_matrix = np.corrcoef(resp_included) # roi x roi
             # set diag to 0
             np.fill_diagonal(correlation_matrix, 0)
             # fischer z transform (arctanh) and append
@@ -110,15 +94,20 @@ for brain_fp in brain_filepaths:
         cmats_iter = np.stack(cmats_iter, axis=2) # roi x roi x iterations
         cmats_sizes.append(cmats_iter)
 
-    cmats_sizes = np.stack(cmats_sizes, axis=3) # roi x roi x iterations x size
-    cmats_pop.append(cmats_sizes)
+    cmats_subsampled.append(np.stack(cmats_sizes, axis=3)) # List of [roi x roi x iterations x size]
     print('Finished brain {} (time = {:.1f} sec)'.format(suffix, time.time()-t0))
 
-d = datetime.datetime.today()
-datestring ='{:02d}'.format(d.year)+'{:02d}'.format(d.month)+'{:02d}'.format(d.day)
+# Full corrmat
+mean_cmat = np.mean(np.stack(cmats_full, axis=2), axis=2) # roi x roi
+np.fill_diagonal(mean_cmat, np.nan)
+CorrelationMatrix_Full = pd.DataFrame(data=mean_cmat, index=name_list_ito, columns=name_list_ito)
 
-cmats_pop = np.stack(cmats_pop, axis=4) # roi x roi x iterations x sizes x brains
-save_fn = os.path.join(analysis_dir, 'subsampled_cmats_{}.npy'.format(datestring))
-np.save(save_fn, (cmats_pop, CorrelationMatrix_Full, subsampled_sizes))
-print('Finished all brains, saved full and population cmats at {}'.format(save_fn))
+RegionResponses.to_pickle(os.path.join(data_dir, 'subsample_CorrelationMatrix_Full.pkl'))
+np.save(os.path.join(data_dir, 'subsample_cmats_full.npy'), (cmats_full)
+print('Saved full cmats to {}'.format(data_dir))
+
+# Subsampled corrmat
+cmats_subsampled = np.stack(cmats_subsampled, axis=4) # roi x roi x iterations x sizes x brains
+np.save(os.path.join(data_dir, 'subsample_cmats_sub.npy'), (cmats_subsampled, subsampled_sizes))
+print('Saved subsampled cmats to {}'.format(data_dir))
 print('Total time = {:.1f} sec)'.format(time.time()-t_total_0))
